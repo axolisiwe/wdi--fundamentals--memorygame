348b7381fa819f846f6fd2f3248f06e6c2c6e8cf
axolisiwe <axolisiwemaholwana4@gmail.com>
2017-07-24 10:08:59 +0200

    finish

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,6 +1,27 @@
- console.log("Up and running!");
+//console.log("Up and running!");
+
+ 
+   console.log("User flipped " + cardOne);
+ var cards = ['queen' + 'queen' + 'king' + 'king'];
+ var cardsInPlay = [];
+
+
  var cardOne = "queen";
+ cardsInPlay.push('queen');
+ console.log("User flipped queen")
+
  var cardTwo = "king";
- var cardThree = "queen";
- var cardFour ="king";
- console.log("User flipped " + cardOne);
+ cardsInPlay.push('king');
+   console.log("User flipped king")
+   if (cardsInPlay.length === 2) {
+   	if (cardsInPlay[0], cardsInPlay[1]);
+   }
+
+    if (cardOne === cardTwo) {
+    
+      alert("You found a match!");
+  } 
+  else {
+      alert("Sorry, try again");
+  }
+
